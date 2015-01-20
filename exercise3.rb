aa53441a25387bee30cbae243abf5e34d1327529
Rolande Saliba <rolande.saliba@gmail.com>
2015-01-20 15:09:54 -0500

    Exercise 3 is done, the year is computed

--- exercise3.rb
+++ exercise3.rb
@@ -1,4 +1,10 @@
 puts "What's your name?"
-name = gets.chomp
+name = gets.chomp.capitalize!
 puts "Hi #{name}!"
+
+# Last step in Exercice 3
+puts "\nHow old are you?"
+age = gets.chomp
+year = 2015 - age.to_i
+puts "You are born in #{year}!"
 
